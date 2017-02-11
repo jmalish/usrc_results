2330503443765ac957fe20d9e90ffac476f500af
Jordan M <jordanmalish@gmail.com>
2017-02-11 15:14:05 -0500

    Remove unneccessary files

--- public/app/results.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Result = (function () {
-    function Result() {
-    }
-    return Result;
-}());
-exports.Result = Result;
-//# sourceMappingURL=results.js.map
--- public/app/sessionDetails.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var SessionDetails = (function () {
-    function SessionDetails() {
-    }
-    return SessionDetails;
-}());
-exports.SessionDetails = SessionDetails;
-//# sourceMappingURL=sessionDetails.js.map
