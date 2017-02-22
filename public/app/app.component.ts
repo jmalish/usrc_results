2e56d5eb63aff4bb61cf993619a1d098c16ef160
Jordan M <jordanmalish@gmail.com>
2017-02-21 22:12:38 -0500

    Starting work on user authentication, WIP

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, enableProdMode} from '@angular/core';
 import {apiURL} from "./myClasses";
+import {AuthService} from "./auth.service";
 
 let isDevMode:boolean = new apiURL().isDevBuild;
 
@@ -11,8 +12,35 @@ if (!isDevMode) {
     moduleId: module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html',
-    styleUrls: ['app.component.css']
+    styleUrls: ['app.component.css'],
+    providers: [AuthService]
 })
 export class AppComponent  {
+    userLoggedIn: boolean;
+    userProfile: Object;
+    userEmail: string;
 
+    constructor(private auth: AuthService) {
+        this.userProfile = JSON.parse(localStorage.getItem('profile'));
+
+        if (this.userProfile === null) {
+            this.userLoggedIn = false;
+        } else {
+            this.userLoggedIn = true;
+            this.userEmail = this.userProfile.email;
+        }
+
+
+        console.log(this.userEmail);
+
+        console.log(this.userLoggedIn);
+    }
+
+    login() {
+        this.auth.login();
+    }
+    logout() {
+        this.auth.logout();
+        location.reload();
+    }
 }
